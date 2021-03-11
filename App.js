0a7d2488349658b0198c169f2044cc28a759743a
ShaneMcNamara <shanemcnamara11@gmail.com>
2021-03-11 10:24:46 -0800

    Repro code for https://github.com/expo/expo/issues/12167

--- App.js
+++ App.js
@@ -1,12 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+import { useAssets } from 'expo-asset';
+import * as ImageManipulator from 'expo-image-manipulator';
 
+const HEIGHT = 60;
 export default function App() {
+  const [assets] = useAssets([require('./assets/badfile.jpeg')]);
+  const [previews,setPreviews] = useState([]);
+  useEffect(() => {
+    const manipulate = async (uris) => {
+      const previews = await Promise.all(uris.map(async (uri) => {
+        const { uri:previewURI } = await ImageManipulator.manipulateAsync(
+            uri,
+            [{ 
+              resize: {
+                height:HEIGHT
+              } 
+            }],
+            {
+              compress:0,
+              format:ImageManipulator.SaveFormat.PNG
+            }
+          );
+          return previewURI;
+        })
+      );
+      setPreviews(previews);
+    };
+    if(assets && assets.length) {
+      const [{ localUri }] = assets;
+      const uriArray = Array(10).fill(localUri);
+      manipulate(uriArray);
+    }
+  },[assets]);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {previews.map((preview,index) => 
+        <Image
+          key={index}
+          source={{ uri:preview }}
+          style={{
+            height:HEIGHT,
+            width:HEIGHT
+          }}
+        />
+      )}
     </View>
   );
 }
