23c424dd4b372ef435a74fca88c207d0cf155599
ShaneMcNamara <shanemcnamara11@gmail.com>
2021-03-11 10:36:36 -0800

    Serialize manipulates

--- App.js
+++ App.js
@@ -4,36 +4,53 @@ import { useAssets } from 'expo-asset';
 import * as ImageManipulator from 'expo-image-manipulator';
 
 const HEIGHT = 60;
+let gated = false;
 export default function App() {
   const [assets] = useAssets([require('./assets/badfile.jpeg')]);
   const [previews,setPreviews] = useState([]);
-  useEffect(() => {
-    const manipulate = async (uris) => {
-      const previews = await Promise.all(uris.map(async (uri) => {
-        const { uri:previewURI } = await ImageManipulator.manipulateAsync(
-            uri,
-            [{ 
-              resize: {
-                height:HEIGHT
-              } 
-            }],
-            {
-              compress:0,
-              format:ImageManipulator.SaveFormat.PNG
-            }
-          );
-          return previewURI;
-        })
+  const [uris,setUris] = useState([]);
+  const manipulate = async (index) => {
+    console.log('manipulating',index)
+      const { uri:previewURI } = await ImageManipulator.manipulateAsync(
+        uris[index],
+        [{ 
+          resize: {
+            height:HEIGHT
+          } 
+        }],
+        {
+          compress:0,
+          format:ImageManipulator.SaveFormat.PNG
+        }
       );
-      setPreviews(previews);
-    };
+      setPreviews([...previews,previewURI]);
+  };
+
+  useEffect(() => {
     if(assets && assets.length) {
       const [{ localUri }] = assets;
-      const uriArray = Array(10).fill(localUri);
-      manipulate(uriArray);
+      const uriArray = Array(20).fill(localUri);
+      setUris(uriArray);
     }
   },[assets]);
 
+  useEffect(() => {
+    if(uris && uris.length) {
+      if(!gated) {
+        manipulate(0);
+        gated = true;
+      }
+    }
+  },[uris]);
+
+  useEffect(() => {
+    const lPrev = previews.length;
+    if(lPrev < uris.length) {
+      setTimeout(() => {
+        manipulate(lPrev);
+      },5000);
+    }
+  },[previews]);
 
   return (
     <View style={styles.container}>
